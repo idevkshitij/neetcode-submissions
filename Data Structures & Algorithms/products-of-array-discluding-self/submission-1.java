f0894a700d624427af5efdd6e3dd4144feeefdfc
Kshitij Shrivastava <74612148+idevkshitij@users.noreply.github.com>
2026-05-11 17:42:11 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        //brute force soln
+        // return bruteForceSol(nums);
+        
+        //optimized sol1
+        return optimizedSol1(nums);
+        //optimized sol2
+    }
+
+    public int[] bruteForceSol(int[] nums){
+        int[] res = new int[nums.length];
+        for(int index = 0; index < nums.length; index++){
+            int currIndexProduct = 1;
+            for(int otherIndex = 0; otherIndex < nums.length; otherIndex++){
+                if(otherIndex == index) continue;
+                currIndexProduct = currIndexProduct * nums[otherIndex];
+            }
+            res[index] = currIndexProduct;
+        }
+        return res;
+    }
+
+    public int[] optimizedSol1(int[] nums){
+        int res[] = new int[nums.length];
+        int totalProduct = 1;
+        int totalZeros = 0;
+        for(int element : nums){
+            if(element == 0) {
+                totalZeros++;
+              continue;  
+            } 
+            totalProduct = totalProduct * element;
+        }
+        //case 1 : more than 1 zeros
+        if(totalZeros > 1){
+            return res;
+        }
+        //case 2 : excatly 1 zero
+        //case 3 : no zeros
+        for(int index = 0; index < nums.length; index++){
+            if(totalZeros == 1){
+                res[index] = (nums[index] == 0) ? totalProduct : 0;
+            }
+            else
+                res[index] = totalProduct / nums[index];
+        }
+
+
+        return res;
+    }
+}  
