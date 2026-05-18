171f89c3f2bed534d982a707e629d5f8841b5e58
Kshitij Shrivastava <74612148+idevkshitij@users.noreply.github.com>
2026-05-18 17:18:26 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        //bruteforce
+        // return bruteforceSol(nums);
+
+        //optimized
+        return optimizedSol(nums);
+    }
+
+    //TC:O(n^3), SC:O(1)
+    public int bruteforceSol(int[] nums) {
+        int maxStreakLen = 0;
+        for(int index = 0; index < nums.length; index++){
+            int streakLen = 1;
+            int currNum = nums[index];
+
+            while(containsElement(currNum + 1, nums)){
+                streakLen++;
+                currNum++;
+            }
+            maxStreakLen = Math.max(maxStreakLen, streakLen);
+        }
+        return maxStreakLen;
+    }
+
+    public boolean containsElement(int element, int[] nums){
+        for(int item : nums){
+            if(item == element) return true;
+        }
+        return false;
+    }
+
+    //TC : O(n), SC : O(n)
+    public int optimizedSol(int nums[]) {
+        int maxStreakLen = 0;
+        //use a hashset for constant time contains check
+        Set<Integer> set = new HashSet<>();
+        for(int element : nums){
+            set.add(element);
+        }
+
+        for(int element : nums) {
+            int streakLen = 1;
+            //check for streak for element only if its the starting element of the streak
+            if(!(set.contains(element-1))){
+                while(set.contains(element + 1)){
+                    streakLen++;
+                    element++;
+                }
+                maxStreakLen = Math.max(maxStreakLen, streakLen);
+            }
+        }
+
+        return maxStreakLen;
+    }
+}
